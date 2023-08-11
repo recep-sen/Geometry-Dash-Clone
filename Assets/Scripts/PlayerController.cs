ef2152b864ad44f3d10f06c057521b92cb041c7f
recep-sen <devrecepsen@gmail.com>
2023-08-11 12:16:52 +0300

    Player ground check and rotation of model added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Variables")]
     public float MoveSpeed;
     public float JumpMultiplier;
+    public float GroundedCheckRayLenght;
+    public LayerMask GroundLayerMask;
+    [Tooltip("This only affects visually")] public float RotationSpeed;
+
+    [Header("References")]
+    public Transform Model;
+
     private Rigidbody2D rb;
+    private bool isGrounded => Physics2D.Raycast(transform.position, Vector2.down, GroundedCheckRayLenght, GroundLayerMask).collider != null;
 
     private void Start()
     {
@@ -18,9 +27,26 @@ public class PlayerController : MonoBehaviour
     {
         transform.Translate(Vector3.right * MoveSpeed * Time.deltaTime);
 
-        if (Input.GetMouseButtonDown(0))
+        if(isGrounded)
         {
-            rb.AddForce(Vector2.up * JumpMultiplier, ForceMode2D.Impulse);
+            Vector3 modelRotation = Model.rotation.eulerAngles;
+            modelRotation.z -= modelRotation.z % 90;
+            Model.rotation = Quaternion.Euler(modelRotation);
+            if (Input.GetMouseButton(0))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * JumpMultiplier, ForceMode2D.Impulse);
+            }
         }
+        else
+        {
+            Model.Rotate(Vector3.back * RotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3.down * GroundedCheckRayLenght));
     }
 }
