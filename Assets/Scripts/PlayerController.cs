13b92b3bbfcd067bbb658c267ca09a311e6c5d7b
recep-sen <devrecepsen@gmail.com>
2023-08-11 11:27:52 +0300

    Jumping added to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     public float MoveSpeed;
+    public float JumpMultiplier;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         transform.Translate(Vector3.right * MoveSpeed * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(Vector2.up * JumpMultiplier, ForceMode2D.Impulse);
+        }
     }
 }
